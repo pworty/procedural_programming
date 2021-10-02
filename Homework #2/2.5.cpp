90c5c5de8ad92c7a7743151a83440c85366029dd
pworty <47080774+pworty@users.noreply.github.com>
2021-10-02 06:30:55 +0300

    Create 2.5.cpp

---
+++ Homework #2/2.5.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <math.h>
+#include <cmath>
+#include <stdlib.h>
+using namespace std;
+
+int main()
+{	
+	float x;
+	float y;
+	float b;
+	float z;
+	cout << "Input x, y, b\n";
+	cin >> x, y, b;
+
+	if (b - y <= 0){
+		cout << "Error: ln(b - y), (b - y)  <= 0\n";
+		return 0;
+	}
+	if (b - x < 0){
+		cout << "Error: root from (b - x), (b - x) < 0\n";
+		return 0;
+	}
+
+	z = log(b - y)*pow(b - x, 0.5);
+	cout << z << "\n";
+
+	return 0;
+}
