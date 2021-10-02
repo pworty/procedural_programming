2b2032415b8b1dcdfc512c49888a158df1c505bf
pworty <47080774+pworty@users.noreply.github.com>
2021-10-02 06:27:01 +0300

    Create 2.1.cpp

---
+++ Homework #2/2.1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <math.h>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	float R;
+	float r;
+	float h;
+	float l;
+	cout << "Input R, r, h\n";
+	cin >> R, r, h;
+	l = pow(pow((R - r), 2) + pow(h, 2), 0.5);
+
+	if (R <= 0 or r <= 0 or r == R or r > R or h <= 0 or l <= 0){
+		cout << "Invalid values\n";
+		return 0;
+	}
+
+
+	float V = 1.0/3.0*M_PI*h*(pow(R, 2) + R*r + pow(r, 2));
+	float S = M_PI*(pow(R, 2) + (R + r)*l + pow(r, 2));
+
+	cout << "Volume=" << V << "\n";
+	cout << "Surface area=" << S << "\n";
+
+	return 0;
+}
