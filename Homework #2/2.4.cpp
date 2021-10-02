2dd466675737c6020a9b8f3e46442cfd57aa01e0
pworty <47080774+pworty@users.noreply.github.com>
2021-10-02 06:33:16 +0300

    Create 2.4.cpp

---
+++ Homework #2/2.4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+#include <cmath>
+#include <stdlib.h>
+using namespace std;
+
+int main()
+{
+	float x, y;
+	for (x = -4.0; x <=4.0; x += 0.5){
+		y = (pow(x, 2) - 2*x + 2.0)/(x - 1.0);
+		cout << y << "\t";
+	}
+	cout << "\n";
+	return 0;
+}
