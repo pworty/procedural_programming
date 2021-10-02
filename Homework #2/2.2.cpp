6fb474b1002970edede4e802ca3c69dec2af198f
pworty <47080774+pworty@users.noreply.github.com>
2021-10-02 06:48:35 +0300

    Update 2.2.cpp

--- Homework #2/2.2.cpp
+++ Homework #2/2.2.cpp
@@ -6,19 +6,27 @@ using namespace std;
 
 int main()
 {
-	int N, n;
-	cout << "Input N\n";
-	cin >> N;
-	
-	if (int(N) == N)
-		n = N;
-	else
-		n = int(N) + 1;
+    float x, a, w;
+    cout << "Input x\n";
+    cin >> x;
 
-	for (int i = 0; i <= 10; i++){
-		cout << n;
-		n++;
-	}
+    if (x == 0){
+        cout << "Error: lg 0\n";
+        return 0;
+    }
 
-	return 0;
+    cout << "Input a\n";
+    cin >> a;
+
+    if (abs(x) < 1)
+        w = a*log(abs(x));
+    else
+    if (a - pow(x, 2) < 0){
+        cout << "Error: root from (a - x^2) < 0\n";
+        return 0;
+    }
+    else
+        w = pow(a - pow(x, 2), 0.5);
+    cout << "w = " << w << "\n";
+    return 0;
 }
