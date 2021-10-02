8a7c9319b69a69a91667ba7dfb87c7deeb9d3d99
pworty <47080774+pworty@users.noreply.github.com>
2021-10-02 06:42:34 +0300

    Update 2.1.cpp

--- Homework #2/2.1.cpp
+++ Homework #2/2.1.cpp
@@ -5,22 +5,22 @@ using namespace std;
 
 int main()
 {
-	float R, r, h, l;
-	cout << "Input R, r, h\n";
-	cin >> R >> r >> h;
-	l = pow(pow((R - r), 2) + pow(h, 2), 0.5);
+    float R, r, h, l;
+    cout << "Input R, r, h\n";
+    cin >> R >> r >> h;
+    l = pow(pow((R - r), 2) + pow(h, 2), 0.5);
 
-	if (R <= 0 or r <= 0 or r == R or r > R or h <= 0 or l <= 0){
-		cout << "Invalid values\n";
-		return 0;
-	}
+    if (R <= 0 or r <= 0 or r == R or r > R or h <= 0 or l <= 0){
+        cout << "Invalid values\n";
+        return 0;
+    }
 
 
-	float V = 1.0/3.0*M_PI*h*(pow(R, 2) + R*r + pow(r, 2));
-	float S = M_PI*(pow(R, 2) + (R + r)*l + pow(r, 2));
+    float V = 1.0/3.0*M_PI*h*(pow(R, 2) + R*r + pow(r, 2));
+    float S = M_PI*(pow(R, 2) + (R + r)*l + pow(r, 2));
 
-	cout << "Volume=" << V << "\n";
-	cout << "Surface area=" << S << "\n";
+    cout << "Volume = " << V << "\n";
+    cout << "Surface area = " << S << "\n";
 
-	return 0;
+    return 0;
 }
